eed7a675467a09a281dfce2b15b216ab2c0502ac
MirzaAyon <ayonmirza98@gmail.com>
2022-06-10 00:03:50 +0600

    foreach added

---
+++ Function-Advanced-Javascript-ES6/foreach.js
@@ -0,0 +1,28 @@
+//push,pop,stack,queue,length,delete,concat,splice(),slice() --- egula nije dekhbo
+//for each,map,filter ekhon dekhbo
+
+
+//forEach 
+const friends = ['neer', 'munna', 'shammi', 'plabon'];
+
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(`Hello ${friends[i]}`);
+// }
+
+//using forEach //and arrow func
+// friends.forEach((frield) => console.log(`Hello ${frield}`));
+
+//regular function use kore
+
+// friends.forEach(function (frield) {
+//     console.log(`Hello ${frield}`);
+// });
+
+friends.forEach(function (x) {
+    console.log(`Hello ${x}`);
+});
+ //x ekhane parameter
+//for each ta amra object eo forloop er poriborte use korte parbo
+
+
+
